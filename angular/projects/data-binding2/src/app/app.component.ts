81ff39834aa914ef96ab2948d7091bac41fbaf63
prasad-bigdp <bigdp585@gmail.com>
2024-01-11 08:51:34 +0530

    update

---
+++ angular/projects/data-binding2/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'data-binding2';
+  name = 'something';
+  inp!:string;
+  imgUrl =
+    'https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=';
+  isDark = true
+  isMain = true
+  something = true;
+  prasad = "uppercase"
+  myColor = 'blue'
+  noColor = ''
+  myMethod(): void {
+    console.log("hellooooooooooooooooooooooooo")
+    if (this.isDark) {
+      this.isDark = false
+    }
+    else {
+      this.isDark = true
+    }
+  }
+  inputChange(e:Event) {
+    this.inp = (e.target as HTMLInputElement).value;
+    console.log(this.inp)
+  }
+}
